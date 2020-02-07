9b875642cd675d8c3b570cc9e42ca24258c0187b
newnyc <kamaunewton78@gmail.com>
2020-02-07 11:11:57 +0300

    refactored the views names

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,21 +1,20 @@
 @extends('layout')
-@yield('header')
+
 @section('content')
     <div id="wrapper">
             <div id="page" class="container">
-
                 @foreach($article as $article )
                     <div class="content">
                         <div class="title">
                             <h3>
-                                <a  href="/articles/{{$article->id}}}">{{ $article->title }}
+                            <a  href="{{ route('articles.show', $article)}}">
+                                    {{ $article->title }}
                                 </a>
                             </h3>
                             <p>
                           <img src="images/banner.jpg" alt="" class="image image-full">
-
                             </p>
-                {{$article->body}}
+                       {{$article->body}}
                         </div>
                         @endforeach
                     </div>
